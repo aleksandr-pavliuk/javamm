f2e5e1df591b4a2e32f2c17d93cbd2fbb382ee1d
aleksandr-pavliuk <a.pavliuk2@gmail.com>
2020-06-15 19:36:17 +0300

    implement base expressions

--- code/src/main/java/module-info.java
+++ code/src/main/java/module-info.java
@@ -6,4 +6,7 @@ module javamm.code {
     exports academy.devonline.javamm.code.fragment;
     exports academy.devonline.javamm.code.fragment.operation;
     exports academy.devonline.javamm.code.exception;
+    exports academy.devonline.javamm.code.fragment.expression;
+    exports academy.devonline.javamm.code.component;
+    exports academy.devonline.javamm.code.syntax;
 }
